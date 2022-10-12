8ef26719f27832a9594892e2140daa69ab6fc7d9
joabsena <joabarbosa1@hotmail.com>
2022-10-12 20:28:09 -0300

    chore: Adjusted config type storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,7 +14,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "production") {
-      config.base = "ignite-lab-design-system";
+      config.base = "/ignite-lab-design-system/";
     }
 
     return config;
